f4822f5cbed8ac10a8e1337176ac95db7653b58f
Tadas Vilkeliskis <vilkeliskis.t@gmail.com>
2012-06-02 21:12:56 -0400

    Remove temp test. Add RequestRouter test.

---
+++ tests/unit/http_request_router_test.cc
@@ -0,0 +1,30 @@
+#include <gtest/gtest.h>
+
+#include "http_request_router.h"
+
+class bolt::network::http::IncommingConnection;
+
+void handler_A(bolt::network::http::IncommingConnection *c)
+{
+    return;
+}
+
+void handler_B(bolt::network::http::IncommingConnection *c)
+{
+    return;
+}
+
+
+TEST(HttpRequestRouterTest, RoutesTest)
+{
+    bolt::network::http::RequestRouter router;
+    bolt::network::http::RequestRouter::request_handler_t handler;
+
+    ASSERT_TRUE(router.route("/some/route"));
+    router.set_route("/A", handler_A);
+    handler = router.route("/A");
+    ASSERT_TRUE(handler_A == handler);
+    router.set_route("/A", handler_B);
+    handler = router.route("/A");
+    ASSERT_TRUE(handler_B == handler);
+}
--- tests/unit/http_request_test.cc
+++
@@ -1,8 +0,0 @@
-#include <gtest/gtest.h>
-
-#include "http_request.h"
-
-TEST(HttpRequestTest, Constructor)
-{
-    bolt::network::http::Request request;
-}
