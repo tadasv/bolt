671c84bcf55290219e8157c0c3043fda4013fa4d
Tadas Vilkeliskis <vilkeliskis.t@gmail.com>
2012-06-10 17:19:25 -0400

    utility to convert basic datatypes to strings.

---
+++ src/string_utils.h
@@ -0,0 +1,36 @@
+// Copyright Tadas Vilkeliskis.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to permit
+// persons to whom the Software is furnished to do so, subject to the
+// following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
+// NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+// USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+#ifndef __BOLT_STRING_UTILS_H__
+
+#define __BOLT_STRING_UTILS_H__
+
+#include <sstream>
+
+template<class T>
+std::string t_to_string(T i)
+{
+    std::stringstream ss;
+    ss << i;
+    return ss.str();
+}
+
+#endif // end of include guard: __BOLT_STRING_UTILS_H__
